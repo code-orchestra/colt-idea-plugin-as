ee8798b44eb435cc06bb722f440b238db2567776
Alexander Eliseyev <a.a.eliseyev@gmail.com>
2013-07-23 13:09:14 +0700

    [COLT-282]

--- src/codeOrchestra/plugin/actions/COLTLaunchAction.java
+++ src/codeOrchestra/plugin/actions/COLTLaunchAction.java
@@ -2,9 +2,12 @@ package codeOrchestra.plugin.actions;
 
 import codeOrchestra.plugin.launch.COLTLauncher;
 import codeOrchestra.plugin.launch.COLTPathNotConfiguredException;
+import com.intellij.execution.ExecutionException;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.ui.Messages;
+
+import java.io.IOException;
 
 /**
  * @author Alexander Eliseyev
@@ -18,10 +21,13 @@ public class COLTLaunchAction extends AnAction {
         } catch (COLTPathNotConfiguredException e) {
             Messages.showErrorDialog("COLT location not configured.\nTo specify the COLT path, go to Settings -> COLT", COLTRemoteAction.COLT_TITLE);
             return;
+        } catch (ExecutionException e) {
+            Messages.showErrorDialog("Can't start COLT:\n" + e.getMessage(), COLTRemoteAction.COLT_TITLE);
+            return;
+        } catch (IOException e) {
+            Messages.showErrorDialog("Can't start COLT:\n" + e.getMessage(), COLTRemoteAction.COLT_TITLE);
+            return;
         }
-
-        // Report launch
-        // TODO: implement
     }
 
 }
--- src/codeOrchestra/plugin/launch/COLTLauncher.java
+++ src/codeOrchestra/plugin/launch/COLTLauncher.java
@@ -1,18 +1,66 @@
 package codeOrchestra.plugin.launch;
 
 import codeOrchestra.plugin.COLTSettings;
+import com.intellij.execution.ExecutionException;
+import com.intellij.openapi.util.SystemInfo;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.io.IOException;
 
 /**
  * @author Alexander Eliseyev
  */
 public final class COLTLauncher {
 
-    public static void launch() throws COLTPathNotConfiguredException {
+    public static Process launch() throws COLTPathNotConfiguredException, ExecutionException, IOException {
         if (!COLTSettings.getInstance().isCOLTPathValid()) {
             throw new COLTPathNotConfiguredException();
         }
 
-        // TODO: implement
+        String coltExecutablePath = completeCOLTPath(completeCOLTPath(COLTSettings.getInstance().getCOLTPath()));
+        return new ProcessBuilder(coltExecutablePath).start();
+    }
+
+    private static String completeCOLTPath(String coltPath) throws ExecutionException {
+        File coltFile = new File(coltPath);
+        if (!coltFile.exists()) {
+            throw new ExecutionException("Can't locate COLT under " + coltPath);
+        }
+
+        String result = coltPath;
+        if (SystemInfo.isMac) {
+            if (coltFile.isDirectory()) {
+                File executableDir = new File(coltFile, "COLT.app/Contents/MacOS");
+                if (!(executableDir.exists())) {
+                    throw new ExecutionException("Can't locate COLT under " + coltPath);
+                }
+
+                File[] files = executableDir.listFiles(new FilenameFilter() {
+                    public boolean accept(File file, String fileName) {
+                        return fileName.toLowerCase().contains("eclipse");
+                    }
+                });
+                if (files == null || files.length == 0) {
+                    throw new ExecutionException("Can't locate COLT under " + coltPath);
+                }
+
+                result = files[0].getPath();
+            }
+        } else if (SystemInfo.isWindows) {
+            result = result + "\\" + "COLT.exe";
+        } else {
+            // Do nothing 
+        }
+
+        return protect(result);
+    }
+
+    public static String protect(String result) {
+        if (result.contains(" ")) {
+            return "\"" + result + "\"";
+        }
+        return result;
     }
 
 }
